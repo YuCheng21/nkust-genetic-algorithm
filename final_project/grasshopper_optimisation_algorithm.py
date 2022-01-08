18d397aef48b46647e0da5727515f417bcd3c4dc
YuCheng <fyc621@gmail.com>
2022-01-08 12:09:42 +0800

    adjust: final_project - trend figure #20220108001

--- final_project/grasshopper_optimisation_algorithm.py
+++ final_project/grasshopper_optimisation_algorithm.py
@@ -1,8 +1,18 @@
 import math
 import random
+import matplotlib.pyplot as plt
+
+"""
+Reference: https://doi.org/10.1016/j.advengsoft.2017.01.004
+Dataset: https://people.sc.fsu.edu/~jburkardt/datasets/knapsack_01/knapsack_01.html
+"""
 
 
 class GrasshopperOptimisationAlgorithm:
+    """
+    Grasshopper Optimisation Algorithm for 01 Knapsack Problem
+    """
+
     def __init__(self, max_iter, c_max, c_min, upper_bound, lower_bound, dimension, f_arg, l_arg):
         # GOA arguments
         self.max_iter = max_iter
@@ -14,6 +24,7 @@ class GrasshopperOptimisationAlgorithm:
         self.f_arg = f_arg
         self.l_arg = l_arg
         # Custom dataset
+        self.__name = None
         self.__capacity = None
         self.__optimal = None
         self.things_information = []
@@ -22,12 +33,23 @@ class GrasshopperOptimisationAlgorithm:
         # Calculate fitness value
         self.punish_fitness_value = 5
         self.best_fitness = None
+        self.current_iteration = None
+        # Plot figure
+        self.history_fitness_value = []
 
     def add_things_information(self, weight, profits):
         self.things_information.append({'weight': weight, 'profits': profits})
 
     def reset_things_information(self):
         self.things_information = []
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
 
     @property
     def capacity(self):
@@ -50,15 +72,19 @@ class GrasshopperOptimisationAlgorithm:
         for i in range(0, quantity):
             self.bags.append({'things': random.choices([0, 1], k=len(self.things_information))})
 
-    def calculate_fitness_value(self):
+    def calculate_fitness_value(self, things: list) -> [int, int]:
+        weight = 0
+        profits = 0
+        for thing_index, thing_value in enumerate(things):
+            if thing_value is 1:
+                weight += self.things_information[thing_index]['weight']
+                profits += self.things_information[thing_index]['profits']
+        return weight, profits
+
+    def get_fitness(self):
         self.best_fitness = {'profits': 0}
         for bag_index, bag_value in enumerate(self.bags):
-            weight = 0
-            profits = 0
-            for thing_index, thing_value in enumerate(bag_value['things']):
-                if thing_value is 1:
-                    weight += self.things_information[thing_index]['weight']
-                    profits += self.things_information[thing_index]['profits']
+            weight, profits = self.calculate_fitness_value(bag_value['things'])
             self.bags[bag_index]['weight'] = weight
             if weight > self.capacity:
                 self.bags[bag_index]['profits'] = self.punish_fitness_value
@@ -67,14 +93,50 @@ class GrasshopperOptimisationAlgorithm:
             if self.bags[bag_index]['profits'] > self.best_fitness['profits']:
                 self.best_fitness = self.bags[bag_index]
 
+    def record_fitness_value(self):
+        buffer = []
+        for bag in self.bags:
+            buffer.append(bag['profits'])
+        self.history_fitness_value.append(buffer)
+
+    def reset_history_fitness(self):
+        self.history_fitness_value = []
+
+    def plot_fitness_trend(self):
+        mean = list(map((lambda x: sum(x) / len(x)), self.history_fitness_value))
+        stddev = list(map((lambda x: math.sqrt(sum((i - sum(x) / len(x)) ** 2 for i in x) / len(x))),
+                          self.history_fitness_value))
+        optimal_weight_value, optimal_profits_value = self.calculate_fitness_value(self.optimal)
+
+        plt.plot(range(1, len(mean) + 1), mean, marker='o', label='mean', linestyle='solid', color='steelblue')
+        plt.plot(range(1, len(stddev) + 1), stddev, marker='o', label='stddev', linestyle='dashed', color='orange')
+        plt.axhline(optimal_profits_value, linestyle='dotted', color='lightcoral')
+        plt.xlabel('Iteration')
+        plt.ylabel('Fitness value')
+        plt.title(f'GOA, Dataset={self.name}, Optimal fitness={optimal_profits_value}')
+        plt.legend(loc='upper left')
+        plt.show()
+        print('===================================================')
+        print('Optimal fitness: ')
+        print(f'thing: {GOA.optimal}, weight: {optimal_weight_value}, profits: {optimal_profits_value}')
+        print('\n\n\n\n')
+
+    def print_current_state(self):
+        print('===================================================')
+        print(f'[{self.current_iteration}]')
+        # print(*self.bags, sep='\n')
+        print(self.best_fitness)
+
     def iterator(self):
+        print(f'[{self.name}]')
         space = (self.upper_bound - self.lower_bound) / 2
-        for current_iter in range(self.max_iter):
-            self.calculate_fitness_value()
-            print('====================')
-            # print(*self.bags, sep='\n')
-            print(self.best_fitness)
-            c_value = self.c_max - (self.c_max - self.c_min) * (current_iter / self.max_iter)
+        self.reset_history_fitness()
+        for current_iteration in range(self.max_iter):
+            self.current_iteration = current_iteration
+            self.get_fitness()
+            self.record_fitness_value()
+            self.print_current_state()
+            c_value = self.c_max - (self.c_max - self.c_min) * (current_iteration / self.max_iter)
             next_iter_bags = []
             for i_bag_index, i_bag_value in enumerate(self.bags):
                 sub_summation = []
@@ -86,14 +148,14 @@ class GrasshopperOptimisationAlgorithm:
                         vector = j_bag_value['things'][index] - i_bag_value['things'][index]
                         distance = abs(vector)
                         social_interaction = self.f_arg * math.exp(-distance / self.l_arg) - math.exp(-distance)
-                        if distance == 0:
-                            buffer.append(0)
-                        else:
+                        try:
                             buffer.append(c_value * space * social_interaction * vector / distance)
+                        except ZeroDivisionError:
+                            buffer.append(0)
                     sub_summation.append(buffer)
-                sub_summation_t = [list(row) for row in zip(*sub_summation)]
+                sub_summation_transpose = [list(row) for row in zip(*sub_summation)]
                 result = []
-                for row_index, row_value in enumerate(sub_summation_t):
+                for row_index, row_value in enumerate(sub_summation_transpose):
                     summation = 0
                     for col in row_value:
                         summation += col
@@ -117,8 +179,10 @@ kwargs = {
     'l_arg': 2
 }
 GOA = GrasshopperOptimisationAlgorithm(**kwargs)
+print(GOA.__doc__)
 
-# # ============= Use P01 Dataset =============
+# ============= Use P01 Dataset =============
+GOA.name = 'P01'
 GOA.capacity = 165
 GOA.optimal = [1, 1, 1, 1, 0, 1, 0, 0, 0, 0]
 GOA.reset_things_information()
@@ -138,21 +202,22 @@ GOA.generate_random_data(30)
 
 # Start GOA
 GOA.iterator()
-print('\noptimal: ', GOA.optimal, sep='\n')
+GOA.plot_fitness_trend()
 
 # ============= Use P02 Dataset =============
-# GOA.capacity = 26
-# GOA.optimal = [0, 1, 1, 1, 0]
-# GOA.reset_things_information()
-# GOA.add_things_information(12, 24)
-# GOA.add_things_information(7, 13)
-# GOA.add_things_information(11, 23)
-# GOA.add_things_information(8, 15)
-# GOA.add_things_information(9, 16)
-#
-# # Generate random data
-# GOA.generate_random_data(50)
-#
-# # Start GOA
-# GOA.iterator()
-# print('\noptimal: ', GOA.optimal, sep='\n')
+GOA.name = 'P02'
+GOA.capacity = 26
+GOA.optimal = [0, 1, 1, 1, 0]
+GOA.reset_things_information()
+GOA.add_things_information(12, 24)
+GOA.add_things_information(7, 13)
+GOA.add_things_information(11, 23)
+GOA.add_things_information(8, 15)
+GOA.add_things_information(9, 16)
+
+# Generate random data
+GOA.generate_random_data(30)
+
+# Start GOA
+GOA.iterator()
+GOA.plot_fitness_trend()
