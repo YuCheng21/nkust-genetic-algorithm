2d5c4c53ab75c64b29a1e54dc8df42045356660e
YuCheng <fyc621@gmail.com>
2022-01-08 21:28:15 +0800

    adjust: final_project #20220108002

--- final_project/grasshopper_optimisation_algorithm.py
+++ final_project/grasshopper_optimisation_algorithm.py
@@ -185,6 +185,7 @@ print(GOA.__doc__)
 GOA.name = 'P01'
 GOA.capacity = 165
 GOA.optimal = [1, 1, 1, 1, 0, 1, 0, 0, 0, 0]
+GOA.punish_fitness_value = 5
 GOA.reset_things_information()
 GOA.add_things_information(23, 92)
 GOA.add_things_information(31, 57)
@@ -208,6 +209,7 @@ GOA.plot_fitness_trend()
 GOA.name = 'P02'
 GOA.capacity = 26
 GOA.optimal = [0, 1, 1, 1, 0]
+GOA.punish_fitness_value = 5
 GOA.reset_things_information()
 GOA.add_things_information(12, 24)
 GOA.add_things_information(7, 13)
